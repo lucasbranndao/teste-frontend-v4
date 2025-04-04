4bb416dcdc3977045830d6e0b6482ce6862c9fb1
Lucas Brandão <lucasbranndao@gmail.com>
2025-04-04 10:14:20 -0300

    hotfix(): remove app-equipamentos

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EquipamentosComponent } from './funcionalidades/equipamentos/equipamentos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EquipamentosComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'app';
